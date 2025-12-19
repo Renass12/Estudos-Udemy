e73ccb5db0ca6738a5f4cb7d42788e77bd494d1e
Renass12 <renanmoour@gmail.com>
2025-12-19 11:30:25 -0300

    Project created

---
+++ src/main/java/com/EstudoUdemy/demo/ProjetinhofelasApplication.java
@@ -0,0 +1,13 @@
+package com.EstudoUdemy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetinhofelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetinhofelasApplication.class, args);
+	}
+
+}
